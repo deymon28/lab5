6b8ea95cece5d1bfb9c67ee175fb2bf4f5126075
deymon28 <66688340+deymon28@users.noreply.github.com>
2020-06-09 18:46:16 +0300

    Add files via upload

---
+++ Letter.java
@@ -0,0 +1,12 @@
+public class Letter {
+	private char letter;
+	public Letter(char a) {
+		setLetter(a);
+	}
+	public char getLetter() {
+		return this.letter;
+	}
+	public void setLetter(char a) {
+		this.letter = a;
+	}
+}
